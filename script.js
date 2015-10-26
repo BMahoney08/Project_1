b3319945292af2ee596994bf788ffbd3a2211755
Brendan Mahoney <BMahoney08@gmail.com>
2015-10-26 16:53:44 -0400

    MVP with comments

--- script.js
+++ script.js
@@ -9,37 +9,57 @@ var flashCard = $('.flashCard');
 var incorrectDeck = $('.incorrectDeck');
 var incorrectCards = [];
 
-//Sets the deck to the full deck;
+//Sets the deck to the full deck.
 cards = allCards;
 
-//Starts at the first flashcard in the deck.
-flashCard.html(cards[0][0]);
+//Function to randomize the order of the cards.
+var shuffle = function(array) {
+  var m = array.length;
+  var t;
+  var i;
+  i = Math.floor(Math.random() * m--);
+  t = array[m]
+  array[m] = array[i]
+  array[i] = t;
+}
 
-//Shuffles the deck and starts user from the beginning again.
-var shuffleDeck = function() {
+//Envoking shuffle card function.
+shuffle(cards);
+
+//Function that starts the deck
+var startGame = function() {
   clickCounter = 0;
   flashCard.html(cards[0][0]);
 }
 
+//Envoking function to start the deck of flashcards.
+startGame();
+
 //Checks to see if all the cards have been cycled through.
 var checkDeckShuffle = function() {
   if(clickCounter >= cards.length) {
-    //Selects which deck to use based on user input.
+    //Selects which deck to use (Incorrect/All) based on user input.
     var whichDeck = prompt("To cycle through the incorrect cards please enter 'I'. To cycle through the entire deck gain, please enter 'A'.");
     if(whichDeck === "I") {
-      //Verifies there are more than 0 cards in the incorrect deck.
+      //Verifies there are cards in the incorrect deck. If so, the deck of incorrect cards is shuffled and prepared for the user.
       if(incorrectCards.length > 0) {
         cards = incorrectCards;
-        shuffleDeck();
+        shuffle(cards);
+        startGame();
+        incorrectCards = [];
       }
+      //Alerts the user that there are no incorrect cards.
       else if(incorrectCards.length <= 0) {
-        alert("You got them all right! The entire deck will be reshuffled.")
-        shuffleDeck();
+        alert("You got them all right! The entire deck will be reshuffled.");
+        cards = allCards;
+        startGame();
       }
+      //Shuffles the entire deck if the user selects all cards as next deck.
     }
     else if(whichDeck === "A") {
       cards = allCards;
-      shuffleDeck();
+      shuffle(cards);
+      startGame();
     }
   }
 }
@@ -49,14 +69,14 @@ answer.on("click", function() {
   flashCard.html(cards[clickCounter][1]);
 });
 
-//User selects if they got the answer to the flashcard correct.
+//User selects if they got the correct answer to the flashcard.
 correct.on("click", function() {
   clickCounter += 1;
   checkDeckShuffle();
   flashCard.html(cards[clickCounter][0]);
 });
 
-//User selects if they got the answer to the flashcard incorrect.
+//User selects if they did not get the correct answer to the flashcard.
 incorrect.on("click", function() {
   incorrectCards.push(cards[clickCounter]);
   clickCounter += 1;
